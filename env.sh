529f00b56dcdf8dc1a7fa1574ccd41b3569d42c7
cristina-grosu <cristina.grosu@bigstep.com>
2018-05-08 16:35:50 +0300

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,30 +1,28 @@
-####################### SSH KEY FOR GIT ###################################
-ssh-keygen -t rsa && ssh-keygen -y -f ~/.ssh/id_rsa > ~/.ssh/id_rsa.pub && eval `ssh-agent -s` && ssh-add -k
-####################### To be added to git account settings ################
-
 
 ###################### GIT PARAMETERS #####################################
-export GIT_PARENT_DIR=~
-export GIT_REPO_NAME=#your-repo
-export GIT_BRANCH_NAME=#your-branch
-export GIT_USER=#your-gituser
-export GIT_EMAIL=#your-email
-export GITHUB_ACCESS_TOKEN=#access-token from github developer settings
-export GIT_USER_UPSTREAM=#your-user
+
+
+export PARENT_DIR=GIT_PARENT_DIR
+export REPO_NAME=GIT_REPO_NAME
+export BRANCH_NAME=GIT_BRANCH_NAME
+export USER=GIT_USER
+export EMAIL=GIT_EMAIL
+export ACCESS_TOKEN=GITHUB_ACCESS_TOKEN
+# GITHUB_ACCESS_TOKEN = personal access token from Github developer settings
+export USER_UPSTREAM=GIT_USER_UPSTREAM
 
 
 ############################################################################
 #### DO NOT EDIT BELOW THIS LINE: derived variables
 ############################################################################
 
-export GIT_REMOTE_URL=git@github.com:$GIT_USER/$GIT_REPO_NAME.git
+#export GIT_REMOTE_URL=git@github.com:$GIT_USER/$GIT_REPO_NAME.git
 export GIT_REMOTE_URL_HTTPS=https://github.com/$GIT_USER/$GIT_REPO_NAME.git
 export GIT_REMOTE_UPSTREAM=$GIT_USER_UPSTREAM/$GIT_REPO_NAME
 
 
 ####################### Git Repo where notebooks will be pushed ############
 cd $GIT_PARENT_DIR && git clone $GIT_REMOTE_URL_HTTPS
-
 
 
 ####################### Change in jupyter config ###########################
@@ -34,4 +32,4 @@ fi
 
 echo 'c.NotebookApp.disable_check_xsrf = True' >> ~/.jupyter/jupyter_notebook_config.py
 
-cp $GIT_PARENT_DIR/githubcommit/config ~/.ssh/config
+#cp $GIT_PARENT_DIR/jupyter_github/config ~/.ssh/config
